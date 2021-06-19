6c796a473292308038022e50695285a4980ba973
Nick Chapman <nic@amadido.co.uk>
2021-06-19 15:38:05 +0100

    post3: serial output and unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
     loop {}
 }
 
@@ -15,7 +29,53 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start () -> ! {
 
     println!["Hello World{}", "!"];
-    println!["The numbers are {} and {}", 42, 1.0/3.0];
 
+    #[cfg(test)]
+    test_main();
+
+    println!["The numbers are {} and {}", 42, 1.0/3.0];
     panic!("Run out of things to do in _start");
+    //loop {}
+}
+
+trait Testable {
+    fn run(&self);
+}
+
+impl<T : Fn()> Testable for T {
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]){
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
+}
+
+const DEBUG_EXIT_PORT: u16 = 0xf4;
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    let mut port = Port::new(DEBUG_EXIT_PORT);
+    unsafe {
+        port.write(exit_code as u32)
+    }
 }
